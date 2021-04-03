7d7b4c340fa6997846d26e9ca05ca882f7d58309
Tom Gariepy <tomgariepy@anjuna.io>
2021-04-03 16:11:43 -0700

    Updated work and skills

--- src/app/constants/work.ts
+++ src/app/constants/work.ts
@@ -1,59 +1,68 @@
-import Work from '../interfaces/work.interface';
+import Work from "../interfaces/work.interface";
 
-const WORK: Work[] = [{
-  position: 'Senior Front End Engineer',
-  company: 'insightsoftware',
-  startDate: new Date(2020, 1, 3),
-  endDate: new Date(2020, 5, 30),
-  city: 'San Mateo',
-  state: 'CA',
-  description: [
-    'Work on a web application for business financial planning',
-    'Design Angular common components',
-    'Propose uniform site design for improved user experience'
-  ]
-}, {
-  position: 'Senior Frontend Engineer',
-  company: 'Motion Recruitment',
-  startDate: new Date(2019, 4),
-  endDate: new Date(2020, 0, 31),
-  city: 'Sunnyvale',
-  state: 'CA',
-  description: [
-    'Built web application features for Walmart Labs',
-    'Developed online grocery service using React with Redux',
-    'Provided unit tests to increase code quality',
-  ]
-}, {
-  position: 'Front End Engineer',
-  company: 'Cape',
-  startDate: new Date(2018, 9),
-  endDate: new Date(2019, 2),
-  city: 'Redwood City',
-  state: 'CA',
-  description: [
-    'Streamlined remote control system for flying drones',
-    'Improved site for responsive video streaming',
-    'Upgraded systems to the latest version of Angular',
-    'Built automated tests to run in CircleCI'
-  ]
-}, {
-  position: 'UI Developer',
-  company: 'Snap! Finance',
-  startDate: new Date(2016, 8, 6),
-  endDate: new Date(2018, 9),
-  city: 'Salt Lake City',
-  state: 'UT',
-  description: [
-    'Produced multiple Angular applications for customer use',
-    'Constructed component library for uniform site designs',
-    'Created full-stack application for dynamic AMP pages',
-    'Deployed web applications using Docker',
-    'Lead development teams based on product requirements',
-    'Emphasized user experience when planning features',
-    'Designed online video player for improved ease of use'
-  ]
-}
+const WORK: Work[] = [
+  {
+    position: "Software Developer",
+    company: "Anjuna Security",
+    startDate: new Date(2020, 8),
+    city: "Palo Alto",
+    state: "CA",
+    description: [
+      "Built a visual interface for software security configuration",
+      "Created custom branding settings for business partners",
+      "Improve buyer experience with interactive training tools",
+      "Increase site stability with automated visual testing suite",
+      "Improve viewer retention by reducing site render time",
+    ],
+  },
+  {
+    position: "Senior Front End Engineer",
+    company: "insightsoftware",
+    startDate: new Date(2020, 1, 3),
+    endDate: new Date(2020, 5, 30),
+    city: "San Mateo",
+    state: "CA",
+    description: [
+      "Worked on Angular site for enterprise financial planning",
+      "Propose uniform site design to improve user experience",
+    ],
+  },
+  {
+    position: "Senior Frontend Engineer",
+    company: "Motion Recruitment",
+    startDate: new Date(2019, 4),
+    endDate: new Date(2020, 0, 31),
+    city: "Sunnyvale",
+    state: "CA",
+    description: [
+      "Developed online grocery service using React with Redux",
+      "Updated order fulfillment process to improve experience",
+    ],
+  },
+  {
+    position: "Front End Engineer",
+    company: "Cape",
+    startDate: new Date(2018, 9),
+    endDate: new Date(2019, 2),
+    city: "Redwood City",
+    state: "CA",
+    description: [
+      "Streamlined interface for remotely flying drones",
+      "Improved site experience with responsive video streaming",
+    ],
+  },
+  {
+    position: "UI Developer",
+    company: "Snap! Finance",
+    startDate: new Date(2016, 8, 6),
+    endDate: new Date(2018, 9),
+    city: "Salt Lake City",
+    state: "UT",
+    description: [
+      "Increased business deals with streamlined applications",
+      "Improved site traffic using dynamic AMP pages",
+    ],
+  },
   // , {
   //   position: 'Web Applications Consultant',
   //   company: 'TEC devs',
@@ -67,6 +76,6 @@ const WORK: Work[] = [{
   //     'Team with designers to improve client satisfaction'
   //   ]
   // }
-]
+];
 
-export default WORK
+export default WORK;
